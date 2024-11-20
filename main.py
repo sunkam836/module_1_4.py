75b96f90192297e6fb7aaa1ba5bd6b87b86f8de3
sunkam836 <sunkam836@gmail.com>
2024-11-20 22:46:54 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+my_string = input('')
+str = my_string
+print(len(str))
+print(my_string.upper())
+print(my_string.upper().lower())
+print(my_string.replace(' ',''))
+print(my_string[-1])
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
